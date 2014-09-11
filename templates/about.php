e261026fbaa112931ecf2c8d89db366ff2fdbc80
Paula Clerkin <Paula@Paulas-MacBook-Pro.local>
2014-09-11 17:43:16 +0100

    updated about

--- templates/about.php
+++ templates/about.php
@@ -10,10 +10,10 @@
     </div>
     <div class="row">
       <div class="col-lg-4 col-lg-offset-2">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste suscipit delectus quia omnis dicta labore ea cum placeat aperiam optio, dolores ratione repellendus sequi consequuntur error animi. Obcaecati maiores, eveniet.</p>
+        <p> This is a women in tech day and it will be exciting.</p>
       </div>
       <div class="col-lg-4">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quasi quisquam quos praesentium natus exercitationem labore adipisci corrupti quidem, nihil. Labore repudiandae modi, delectus similique maiores animi qui perspiciatis minima.</p>
+        <p>By Paula :)</p>
       </div>
       <div class="col-lg-8 col-lg-offset-2 text-center">
         <a href="#" class="btn btn-lg btn-outline">
