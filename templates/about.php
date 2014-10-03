380f968efa5d5d0d8231121cda5d291fe5e23199
Paula C <prclerkin@gmail.com>
2014-10-03 21:11:58 +0100

    updated about text

--- templates/about.php
+++ templates/about.php
@@ -10,11 +10,13 @@
     </div>
     <div class="row">
       <div class="col-lg-4 col-lg-offset-2">
-        <p>The University of Nottingham's Women In Tech day is for everyone; tech enthusiasts, programmers or non-programmers, students at college or university, women from all walks of life. </br></br>It's all about women inspiring women. </br></br>There are three parts to the day: talks from some of the most interesting and exciting women within the tech industry, workshops from hands on programming to smaller talks and an afternoon of networking opportunities with some of the best companies around.</p>
+        <p>The University of Nottingham's Women In Technology day is for everyone; tech enthusiasts, programmers or non-programmers, students at college or university, women and men from all walks of life. </br></br>The emphasis: Women inspiring women. </br></br>There will be three parts to the day; talks from some of the most fascinating women within the tech industry, workshops; from hands on programming to smaller talks, and an afternoon of networking opportunities with the best companies around.</p>
       </div>
       <div class="col-lg-4">
-        <p>Only 13% of STEM jobs in the UK are occupied by women and out of all the CS students in the UK, only 18% are women. </br></br>This is why HackSoc and The University of Nottingham have teamed up to inspire and encourage female university and sixth form students to pursue technology as a hobby or career. </br></br>We want women to inspire women; to get students excited by their potential.</p>
+        <p>Only 13% of STEM jobs in the UK are occupied by women and out of all the CS students in the UK, only 18% are women. </br></br>Although we don't like these figures, we don't want to, and we're not going to complain about it. </br></br>We're going to celebrate the individuals behind the figures and we're going to hear their stories.</br></br> HackSoc and The University of Nottingham have teamed up to inspire and encourage female university and sixth form students to pursue technology as a hobby or career. </p>
       </div>
+    <div class="col-lg-12 text-center">
+        <p></br>We want to get students excited by their potential.</p>
 <!--       <div class="col-lg-8 col-lg-offset-2 text-center">
         <a href="#" class="btn btn-lg btn-outline">
           <i class="fa fa-circle"></i>
