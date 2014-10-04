1cdde4eb5f768fd26dcf2f366405c5735a696d41
Jamie Tanna <jamie@jamietanna.co.uk>
2014-10-04 14:07:12 +0100

    About now uses paragraphs

--- templates/about.php
+++ templates/about.php
@@ -10,10 +10,17 @@
     </div>
     <div class="row">
       <div class="col-lg-4 col-lg-offset-2">
-        <p>The University of Nottingham's Women In Technology day is for everyone; tech enthusiasts, programmers or non-programmers, students at college or university, women or men from all walks of life. </br></br>The emphasis: women inspiring women. </br></br>Although technically an event by women, for women, guys are more than welcome to attend. After all, who wouldn't want to be in the presence of such brilliant people?</br></br>There will be three parts to the day; talks from some of the most fascinating women within the tech industry, workshops; from hands on programming to smaller talks, and an afternoon of networking opportunities with the best companies around.</p>
+        <p>The University of Nottingham's Women In Technology day is for everyone; tech enthusiasts, programmers or non-programmers, students at college or university, women or men from all walks of life.</p>
+	<p>The emphasis: women inspiring women. </p>
+	<p>Although technically an event by women, for women, guys are more than welcome to attend. After all, who wouldn't want to be in the presence of such brilliant people?</p>
+	<p>There will be three parts to the day; talks from some of the most fascinating women within the tech industry, workshops; from hands on programming to smaller talks, and an afternoon of networking opportunities with the best companies around.</p>
       </div>
       <div class="col-lg-4">
-        <p>Only 13% of STEM jobs in the UK are occupied by women and out of all the CS students in the UK, only 18% are female. </br></br>Although we don't like these figures, we definitely don't want to, and we're certainly not going to complain about them. </br></br>We're celebrating the individuals behind the figures and we want to hear their stories.</br></br> HackSoc and the University of Nottingham have teamed up to inspire and encourage female university and sixth form students to pursue technology as a hobby or as a career.</br></br>We want to get students excited by their potential.</p>
+        <p>Only 13% of STEM jobs in the UK are occupied by women and out of all the CS students in the UK, only 18% are female. </p>
+	<p>Although we don't like these figures, we definitely don't want to, and we're certainly not going to complain about them. </p>
+	<p>We're celebrating the individuals behind the figures and we want to hear their stories.</p>
+	<p> HackSoc and the University of Nottingham have teamed up to inspire and encourage female university and sixth form students to pursue technology as a hobby or as a career.</p>
+	<p>We want to get students excited by their potential.</p>
       </div>
 <!--       <div class="col-lg-8 col-lg-offset-2 text-center">
         <a href="#" class="btn btn-lg btn-outline">
