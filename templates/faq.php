6dc690332a738f55f196eb6c98b58c1e8b519d91
Jamie Tanna <jamie@jamietanna.co.uk>
2014-10-04 13:11:18 +0100

    Fixed FAQs grid - fit correctly into row

--- templates/faq.php
+++ templates/faq.php
@@ -8,39 +8,44 @@
       </div>
     </div>
 
+    <?php $class = "col-md-4 col-sm-3 col-xs-12"; ?>
 
     <div class="row">
-      <div class="col-md-4 col-sm-1 col-xs-12">
+      <div class="<?php echo $class; ?>">
         <p class="highlight"><strong>What is Women In Tech about?</strong></p>
         <p class="text-muted">Women In Tech is a day to learn about the women currently working in the technology industry. It's aimed at students of any age, from any background who have an interest in technology as a career or as a hobby.</p>
       </div>
 
-      <div class="col-md-4 col-sm-3 col-xs-12">
+      <div class="<?php echo $class; ?>">
         <p class="highlight"><strong>Can guys come too?</strong></p>
         <p class="text-muted">YES! Although the emphasis is on women, the day itself is still a technology conference with amazing speakers, workshops and supporters. It's a valuable opportunity for both genders to listen to some brilliant stories and be inspired.</p>
       </div>
 
-      <div class="col-md-4 col-sm-3 col-xs-12">
+      <div class="<?php echo $class; ?>">
         <p class="highlight"><strong>Where is the Women In Tech day held?</strong></p>
         <p class="text-muted">It will be held at the University of Nottingham's Jubilee Campus. It will be based in the Business School South building and the School of Computer science. </p> 
       </div>
+    </div>
 
-      <div class="col-md-4 col-sm-1 col-xs-1">
+    <div class="row">
+      <div class="<?php echo $class; ?>">
         <p class="highlight"><strong>What does the day include?</strong></p>
         <p class="text-muted">In the morning we have a number of inspirational talks from notable women. After lunch we will have a series of workshops and a tradefair-style opportunity for networking.</p>
       </div>
 
-      <div class="col-md-4 col-sm-3 col-xs-12">
+      <div class="<?php echo $class; ?>">
         <p class="highlight"><strong>What workshops can I take part in?</strong></p>
         <p class="text-muted">Bare with us!</p>
       </div>
 
-      <div class="col-md-4 col-sm-3 col-xs-12">
+      <div class="<?php echo $class; ?>">
         <p class="highlight"><strong>Which companies will be there?</strong></p>
         <p class="text-muted">Coming soon!</p>
       </div>
+    </div>
 
-      <div class="col-md-4 col-sm-3 col-xs-12">
+    <div class="row">
+      <div class="col-md-offset-4 <?php echo $class; ?>">
         <p class="highlight"><strong>Do we get food and drinks?</strong></p>
         <p class="text-muted">We will provide a continental breakfast at registration, refreshments throughout the day and lunch.</p>
       </div>
