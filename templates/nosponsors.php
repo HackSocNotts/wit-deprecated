2f667e091cdd0f70862ccfc222f0e648140c80d7
Jamie Tanna <jamie@jamietanna.co.uk>
2014-10-16 23:14:34 +0100

    Updated sponsors to be centred

--- templates/nosponsors.php
+++ templates/nosponsors.php
@@ -12,16 +12,34 @@
         <h3>Become part of our team</h3></br>
     </div>
 
-    <div>
-      <img src="img/sponsors/Hacksoc">
-      <img src="img/sponsors/codeclub.png">
-      <img src="img/sponsors/capitalone.jpg">
-      <img src="img/sponsors/goldmansachs.png">
-      <img src="img/sponsors/kpmg.png">
-      <img src="img/sponsors/flyhigh.png">
-      <img src="img/sponsors/pixelrift.jpg">
-      <img src="img/sponsors/impression.png">
+    <div class="row">
+      <div class="col-lg-2">
+        <img src="img/sponsors/Hacksoc" />
+      </div>
+      <div class="col-lg-2">
+	<img src="img/sponsors/codeclub.png" />
+      </div>
+      <div class="col-lg-2">
+	<img src="img/sponsors/capitalone.jpg" />
+      </div>
+      <div class="col-lg-2">
+	<img src="img/sponsors/goldmansachs.png" />
+      </div>
+      <div class="col-lg-2">
+	<img src="img/sponsors/kpmg.png" />
+      </div>
+      <div class="col-lg-2">
+	<img src="img/sponsors/flyhigh.png" />
+      </div>
+    </div>
 
+    <div class="row">
+      <div class="col-lg-2 col-lg-offset-4">
+	<img src="img/sponsors/pixelrift.jpg" />
+      </div>
+      <div class="col-lg-2">
+	<img src="img/sponsors/impression.png" />
+      </div>
     </div>
         <div class="text-center"><h4>If you're interested in becoming a sponsor, contact us <a class="email-text" href="mailto:prclerkin@gmail.com">here</a>.</h4></div>
     </div>
