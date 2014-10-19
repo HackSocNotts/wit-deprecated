8b69fd1b0ec11833dadba299b823d208f516aa6d
Jamie Tanna <jamie@jamietanna.co.uk>
2014-10-19 11:59:30 +0100

    tannaman -> jamietanna

--- templates/footer.php
+++ templates/footer.php
@@ -5,7 +5,7 @@
       <h3>&ldquo;Don't tell me the sky is the limit when there are footprints on the moon!&rdquo;</h3>
       <p>&mdash;Paul Brandt</p>
       <p class="copy">Built with &hearts; by <br>
-      <a href="http://www.harrymt.com/" target="_blank">harrymt</a> &amp; <a  target="_blank" href="http://csgirlno1dea.blogspot.co.uk/">kerpowla</a> &amp; <a href="http://jamietanna.co.uk/" target="_blank">tannaman</a> &amp; SpeedoDevo
+      <a href="http://www.harrymt.com/" target="_blank">harrymt</a> &amp; <a  target="_blank" href="http://csgirlno1dea.blogspot.co.uk/">kerpowla</a> &amp; <a href="http://jamietanna.co.uk/" target="_blank">jamietanna</a> &amp; SpeedoDevo
       <br>for <a class="hacksoc" href="http://www.hacksocnotts.co.uk/"><span class="hacksoc_green">ha</span>c<span class="hacksoc_green">kS</span>oc</a>
       </p>
       <ul class="social">
