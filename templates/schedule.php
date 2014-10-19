d6c4ea4cb43598ac9d0d49973dee8c2f4cf9d046
Jamie Tanna <jamie@jamietanna.co.uk>
2014-10-19 12:26:01 +0100

    Hacky PHP added to get schedule format right

--- templates/schedule.php
+++ templates/schedule.php
@@ -9,15 +9,22 @@
       </div>
     </div>
 
+<?php 
+	$classes = array("","");
+	foreach (array("lg", "md", "sm", "xs") as $n) {
+		$classes[0] .= "col-$n-4 col-$n-offset-2 ";
+		$classes[1] .= "col-$n-4 ";
+ 	}
+?>
     <div class="row">
       <div class="text-right">
-      <div class="col-lg-4 col-lg-offset-2">
+      <div class="<?php echo $classes[0]; ?>">
         <p>09:00 - 09:30</p>
         <p>09:30 - 09:50</p>
         <p>09:50 - 11-10</p>
       </div>
     </div>
-      <div class="col-lg-4">
+      <div class="<?php echo $classes[1]; ?>">
         <p>Registration</p>
         <p>Welcome address</p>
         <p>Speakers</p>
@@ -30,11 +37,11 @@
 
     <div class="row">
       <div class="text-right">
-      <div class="col-lg-4 col-lg-offset-2">
+      <div class="<?php echo $classes[0]; ?>">
         <p>11:30 - 12:50</p>
       </div>
     </div>
-      <div class="col-lg-4">
+      <div class="<?php echo $classes[1]; ?>">
         <p>Speakers</p>
       </div>
     </div>
@@ -46,13 +53,13 @@
 
     <div class="row">
       <div class="text-right">
-      <div class="col-lg-4 col-lg-offset-2">
+      <div class="<?php echo $classes[0]; ?>">
         <p>13:30 - 14:30</p>
         <p>14:30 - 15:30</p>
         <p>15:30 - 16:30</p>
       </div>
     </div>
-      <div class="col-lg-4">
+      <div class="<?php echo $classes[1]; ?>">
         <p>Workshops 1</p>
         <p>Workshops 2</p>
         <p>Workshops 3</p>
@@ -65,11 +72,11 @@
 
     <div class="row">
       <div class="text-right">
-      <div class="col-lg-4 col-lg-offset-2">
+      <div class="<?php echo $classes[0]; ?>">
         <p>16:45 - 17:00</p>
       </div>
     </div>
-      <div class="col-lg-4">
+      <div class="<?php echo $classes[1]; ?>">
         <p>Close</p>
       </div>
     </div>
