af5eaeeaad01407e89fe8025c87c7a8f5a762693
Paula C <prclerkin@gmail.com>
2014-11-07 00:10:59 +0000

    added embedded map

--- templates/schedule.php
+++ templates/schedule.php
@@ -75,7 +75,9 @@
         <p>Close</p>
       </div>
     </div>
-
+<div class ="text-center">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2403.820155140037!2d-1.186477000000016!3d52.95165900000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4879c21d258aeb79%3A0x4da28c4722953e75!2sBusiness+School+(South)!5e0!3m2!1sen!2suk!4v1415318770393" width="600" height="450" frameborder="0" style="border:0"></iframe>
+</div>
   </div>
 </section>
 <!-- = schedule.php end = -->
