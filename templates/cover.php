f5e269f0b7f10d3ee365da424bf4cd2fd505d0ae
Kevin Lewis <kevin@jellybabi.es>
2016-10-22 14:58:03 +0100

    Generatively creating hero background, with some interactivity

--- templates/cover.php
+++ templates/cover.php
@@ -1,29 +1,27 @@
 <!-- Header -->
-<header>
-<div class="bg">
-<div class="overlay"></div>
 
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12">
+<header>
+  <canvas id="canvas"></canvas>
+  <div id="altBG"></div>
+  
+    <div class="content">
+      <div class="inner">
         <img class="img-responsive img-logo-heading" src="img/logo-heading.png" alt="">
         <div class="intro">
           <div class="intro-strapline">
             Women inspiring women
           </div>
           <div class="intro-text">a conference for <span class="moving_text">Women</span></div>
-          <!-- <img class="img-responsive img-logo" src="img/logo.png" alt=""> -->
           <div class="location-tag"><span class="tag">University of Nottingham</span></div>
         </div>
         <div class="intro-tags">
           <span class="tag">18</span>/
           <span class="tag">02</span>/
           <span class="tag">2017</span>
-				</div>
-				<div class="location-tag"><span class="tag" style="color:#FFED00">Check back soon for more details!!</span></div>
-        <!--<a class="btn btn-register" href="#register">Register</a>-->
+        </div>
+        <div class="location-tag"><span class="tag" style="color:#FFED00">Check back soon for more details
       </div>
     </div>
   </div>
- </div>
+
 </header>
